d98a4c0b7e196583ce9890c543d82bed69c0130b
Prayush Khadka <khadkaprayush8@gmail.com>
2025-01-01 14:10:46 +0545

    changes

--- first-game/exit.gd
+++
@@ -1,3 +0,0 @@
-extends Button
-func _on_exit_pressed() -> void:
-	get_tree().quit()
--- first-game/start.gd
+++
@@ -1,3 +0,0 @@
-extends Button
-func _on_start_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/Game.tscn")
